893ed7e0f15dfc931aabcbbb3e78dbce517fc09d
Cliff Chao <cliffchao97@gmail.com>
2023-12-12 17:41:35 -0800

    Add playRound and getPlayerChoice function

--- rps.js
+++ rps.js
@@ -6,3 +6,49 @@ function getComputerChoice(){
     console.log(option);
     return option;
 }
+
+function getPlayerChoice(){
+    let playerSelection = prompt("Please select Rock, Paper, or Scissors");
+    return playerSelection;
+}
+
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
+        let res = "Tie! Please try again."
+        return res;
+    }
+    else if (playerSelection.toLowerCase() == "rock"){
+        if (computerSelection == "paper"){
+            let res = "You Lose! Paper beats Rock";
+            return res;
+            console.log(res);
+        }
+        else{
+            let res = "You Win! Rock beats Scissors";
+            console.log(res);
+        }
+    }
+
+    else if (playerSelection.toLowerCase() == "paper"){
+        if (computerSelection == "rock"){
+            let res = "You Win! Paper beats Rock";
+            console.log(res);
+        }
+        else{
+            let res = "You Lose! Scissors beats Paper";
+            console.log(res);
+        }
+    }
+
+    else if (playerSelection.toLowerCase() == "scissors"){
+        if (computerSelection == "rock"){
+            let res = "You Lose! Rock beats Scissors";
+            console.log(res);
+        }
+        else{
+            let res = "You Win! Scissors beats Paper";
+            console.log(res);
+        }
+    }
+}
